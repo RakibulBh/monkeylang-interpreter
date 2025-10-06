6b64d295899ae59efbc1901d9b20f2a556d06028
RakibulBh <rakibulbhuiyan1973@gmail.com>
2025-10-06 21:27:18 +0100

    allow parsing identifiers as tokens

--- internal/lexer/lexer_test.go
+++ internal/lexer/lexer_test.go
@@ -9,6 +9,15 @@ import (
 func TestNextToken(t *testing.T) {
 
 	input := `=+(){},;`
+	_ = `
+	let five  = 5;
+	let ten = 10;
+	let add = fn(x, y) {
+		x + y
+	};
+	let result = add(five, ten)
+	`
+
 
 	type TokenTest struct {
 		expectedType token.TokenType
@@ -16,13 +25,41 @@ func TestNextToken(t *testing.T) {
 	}
 
 	tests := []TokenTest{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
+		{token.LET, "let"},
+        {token.IDENT, "five"},
+        {token.ASSIGN, "="},
+        {token.INT, "5"},
+        {token.SEMICOLON, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "ten"},
+        {token.ASSIGN, "="},
+        {token.INT, "10"},
+        {token.SEMICOLON, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "add"},
+        {token.ASSIGN, "="},
+        {token.FUNCTION, "fn"},
         {token.LPAREN, "("},
+        {token.IDENT, "x"},
+        {token.COMMA, ","},
+		{token.IDENT, "y"},
         {token.RPAREN, ")"},
         {token.LBRACE, "{"},
+        {token.IDENT, "x"},
+        {token.PLUS, "+"},
+        {token.IDENT, "y"},
+        {token.SEMICOLON, ";"},
         {token.RBRACE, "}"},
+        {token.SEMICOLON, ";"},
+        {token.LET, "let"},
+        {token.IDENT, "result"},
+        {token.ASSIGN, "="},
+        {token.IDENT, "add"},
+        {token.LPAREN, "("},
+        {token.IDENT, "five"},
         {token.COMMA, ","},
+        {token.IDENT, "ten"},
+        {token.RPAREN, ")"},
         {token.SEMICOLON, ";"},
         {token.EOF, ""},
 	}
